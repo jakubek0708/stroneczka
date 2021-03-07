b94517628533f8d63beded1a60c797f4e15f1c2a
jakubek0708 <jakubek0708@gmail.com>
2021-03-07 13:26:03 +0100

    testing

--- xd.js
+++ xd.js
@@ -1,2 +1,7 @@
-console.log("czemu tu jestes idz sobie")
-console.log("xd")
+var sauce = [285504, 255244, 297974, 261096, 100823, 158902, 249483, 261735, 338456, 291486];
+var i;
+var len = sauce.length;
+console.log("SAUCE:")
+for (i = 0; i < len; i++) {
+  console.log(sauce[i]);
+}
